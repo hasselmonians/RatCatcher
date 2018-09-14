3d1128396b78f9f57af78fe5f9f2013a8eec39cf
= <alec.hoyland@gmail.com>
2018-09-14 12:43:40 -0400

    Fix extract arguments

--- @RatCatcher/RatCatcher.m
+++ @RatCatcher/RatCatcher.m
@@ -31,7 +31,7 @@ methods (Static)
   % both of these methods naturally sort, that is, by separating the name into syntactic phrases and then sorting hierarchically...for example, 'output-11' comes before 'output-21' when naturally sorted
   [X,ndx,dbg] = natsort(X,xpr,varargin)
   [X,ndx,dbg] = natsortfiles(X,varargin)
-  [analysisObject, dataObject] = extract(dataTable, analysis, index)
+  [analysisObject, dataObject] = extract(dataTable, index, analysis, verbose)
 
 end % static methods
 
