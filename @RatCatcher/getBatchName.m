986a638cc81792846b1e8ab1c96c7beeea07f5d3
Alec Hoyland <alec.hoyland@posteo.net>
2019-03-28 15:32:56 -0400

    Make function more robust against variable argument types

--- @RatCatcher/getBatchName.m
+++ @RatCatcher/getBatchName.m
@@ -1,13 +1,17 @@
 function output = getBatchName(expID, protocol)
   % comes up with a verbose name that unambiguously identifies any output file
 
-  expID  = expID';
-  output = expID{1};
+  if iscell(expID)
+    expID  = expID';
+    output = expID{1};
 
-  for ii = 2:numel(expID)
-    output = [output '-' expID{ii}];
+    for ii = 2:numel(expID)
+      output = [output '-' expID{ii}];
+    end
+
+    output = [output '-' protocol];
+  else
+    output = expID;
   end
-
-  output = [output '-' protocol]
 
 end % function
