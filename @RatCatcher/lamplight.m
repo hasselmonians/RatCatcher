b7ef603a09527833d662b4a0c1e407b925583b92
Alec Hoyland <alec.hoyland@posteo.net>
2019-03-29 17:47:09 -0400

    Add documentation, fix bugs in function

--- @RatCatcher/lamplight.m
+++ @RatCatcher/lamplight.m
@@ -1,6 +1,10 @@
-function lamplight(self, varargin)
+function lamplight(self, batchname, varargin)
 
 	% performs bookkeeping routines based on the protocol
+	% the protocol is read from the RatCatcher object
+	% the batchname is used to check for files with the correct name
+	% variable input arguments are for use before batchify by the user
+	% they are specific to each protocol
 
 	switch self.protocol
 
@@ -8,31 +12,37 @@ function lamplight(self, varargin)
 		% save the KiloPlex.options data structure as a .mat file in localPath
 		% varargin allows modification of the options before saving
 
-		k = KiloPlex();
-
-		if mathlib.iseven(length(varargin))
-			for ii = 1:2:length(varargin)-1
-				temp = varargin{ii};
-				if ischar(temp)
-					if ~any(find(strcmp(temp,fieldnames(k.options))))
-						disp(['Unknown option: ' temp])
-						disp('The allowed options are:')
-						disp(fieldnames(k.options))
-						error('UNKNOWN OPTION')
-					else
-						k.options.(temp) = varargin{ii+1};
+		% check to see if options file already exists
+		if ~exist(fullfile(self.localPath, batchname, 'options.mat'), 'file')
+			% instantiate object with default properties
+			k = KiloPlex();
+			% validate arguments
+			if mathlib.iseven(length(varargin))
+				for ii = 1:2:length(varargin)-1
+					temp = varargin{ii};
+					if ischar(temp)
+						if ~any(find(strcmp(temp,fieldnames(k.options))))
+							disp(['Unknown option: ' temp])
+							disp('The allowed options are:')
+							disp(fieldnames(k.options))
+							error('UNKNOWN OPTION')
+						else
+							k.options.(temp) = varargin{ii+1};
+						end
 					end
 				end
+			else
+				error('Inputs need to be name value pairs')
 			end
-		else
-			error('Inputs need to be name value pairs')
-		end
-
-		k.publish(self.localPath);
+			% save the options.mat file
+			k.publish(fullfile(self.localPath, batchname, 'options.mat'));
+	else
+		disp('[INFO] options file already exists for this batch name')
+	end
 
 	otherwise
 		% do nothing
-		
+
 	end % switch
 
 end % function
