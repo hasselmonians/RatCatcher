7e38d3e7cfbefbe7493060dd434ff04a2af12ee9
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-10 12:44:47 -0400

    Add functions to validate properties of RatCatcher

---
+++ @RatCatcher/getBatchScriptName.m
@@ -0,0 +1,6 @@
+function batchscriptname = getBatchScriptName()
+  % determine the batch script name
+
+    batchscriptname = which('RatCatcher-generic-script.sh');
+
+end
---
+++ @RatCatcher/validate.m
@@ -0,0 +1,100 @@
+function validate(self)
+
+  % validates secondary properties of RatCatcher prior to batchifying
+
+  % batch name
+
+  if isempty(self.batchname)
+    % no additional information specified by user
+    % finding the batch name automatically
+
+    self.batchname = self.getBatchName();
+
+    if self.verbose
+      disp('[INFO] batch name determined automatically')
+    end
+
+  else
+    % batch name was preset by the user
+
+    if self.verbose
+      disp('[INFO] batch name determined by user')
+    end
+
+  end
+
+  % filenames and filecodes
+
+  if isempty(self.filenames) & isempty(self.filecodes)
+    % no additional information specified by user
+    % finding filenames and filecodes automatically
+    [self.filenames, self.filecodes] = self.parse();
+
+    if self.verbose
+      disp('[INFO] filenames and filecodes determined automatically')
+    end
+
+  else
+
+    if self.verbose
+      disp('[INFO] filenames and filecodes determined by user')
+    end
+
+  end
+
+  % batch function name
+
+  if isempty(self.batchfuncname)
+    % no additional information specified by user
+    % finding the batch function name automatically
+    self.batchfuncname = self.getBatchFuncName();
+
+    if isempty(self.batchfuncname)
+      % couldn't find the batch function name automatically
+      % error
+
+      if self.verbose
+        disp('[ERROR] batch function name not found')
+        return
+      end
+
+    else
+      % batch function name was found automatically
+
+      if self.verbose
+        disp(['[INFO] batch function name determined automatically'])
+      end
+
+    end
+
+  else
+    % the batch function name was already provided
+
+    if self.verbose
+      disp(['[INFO] batch function name determined by user'])
+    end
+
+  end
+
+  % batch script name
+
+  if isempty(self.batchscriptname)
+    % batch script is not provided by user
+    % determine automatically
+    self.batchscriptname = self.getBatchScriptName();
+
+    if verbose == true
+      disp(['[INFO] batch script determined automatically'])
+    end
+
+  else
+    if verbose == true
+      disp('[INFO] batch script determined by user')
+    end
+  end
+
+  if numel(which(scriptname)) == 0
+    error(['[ERROR] batch function not found at: ' self.batchscriptname])
+  end
+
+end % function
