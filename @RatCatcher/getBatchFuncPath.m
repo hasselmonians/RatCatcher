ca179a7920b368ba01844e607d4ff5316c938c27
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-12 14:21:29 -0400

    minor fixes

--- @RatCatcher/getBatchFuncPath.m
+++ @RatCatcher/getBatchFuncPath.m
@@ -1,7 +1,7 @@
 function batchfuncpath = getBatchFuncPath(self)
   % acquires the batch function full path
 
-  batchfuncpath = which([protocol '.batchFunction']);
+  batchfuncpath = which([self.protocol '.batchFunction']);
   if numel(batchfuncpath) == 0
     batchfuncpath = [];
   end
