1ed76574f7a0accf8c63b10160c17a37db5c545c
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-22 13:51:23 -0400

    Setup lamplight to write kiloplex channel map

--- @RatCatcher/lamplight.m
+++ @RatCatcher/lamplight.m
@@ -17,37 +17,44 @@ function filepaths = lamplight(self, varargin)
 		% save the KiloPlex.options data structure as a .mat file in localpath
 		% varargin allows modification of the options before saving
 
-		% check to see if options file already exists
-		filename = ['options-' self.batchname '.mat'];
-		filepath = fullfile(self.localpath, filename);
-		filepaths = {filepath};
-		if ~exist(filepath, 'file')
-			% instantiate object with default properties
+		% set up the filepaths
+		options_filename = ['options-' self.batchname '.mat'];
+		channel_filename = ['channel_map-' self.batchname '.mat'];
+		options_filepath = fullfile(self.localpath, options_filename);
+		channel_filepath = fullfile(self.localpath, channel_filename);
+
+		options_exist = exist(options_filepath, 'file');
+		channel_exist = exist(channel_filepath, 'file');
+
+		if ~options_exist | ~channel_exist
+			% instantiate the KiloPlex object
 			k = KiloPlex();
-			% validate arguments
-			if mathlib.iseven(length(varargin))
-				for ii = 1:2:length(varargin)-1
-					temp = varargin{ii};
-					if ischar(temp)
-						if ~any(find(strcmp(temp,fieldnames(k.options))))
-							disp(['Unknown option: ' temp])
-							disp('The allowed options are:')
-							disp(fieldnames(k.options))
-							error('UNKNOWN OPTION')
-						else
-							k.options.(temp) = varargin{ii+1};
-						end
-					end
-				end
-			else
-				error('Inputs need to be name value pairs')
+			k.options.chanMap = channel_filepath;
+			% set options according to key-value arguments
+			k.options = validateArgs(k.options, varargin);
+
+			if ~exist(self.localpath, 'file')
+				mkdir(self.localpath)
 			end
-			% save the options.mat file
-			k.publish(self.localpath, filename);
-			disp(['[INFO] options file saved at: ' filepath])
-	else
-		disp('[INFO] options file already exists for this batch name')
-	end
+
+			if ~options_exist
+				% create options file
+				options = k.options;
+				save(options_filepath, 'options');
+			end
+
+			if ~channel_exist
+				% create channel map file
+				Nchannels = options.NchanTOT;
+				connected = true(Nchannels,1);
+				chanMap 	= 1:NChannels;
+				chanMap0ind = chanMap - 1;
+				xcoords 	= corelib.vectorise(repmat([1 2 3 4]', 1, Nchannels/4));
+				ycoords 	= corelib.vectorise(repmat(1:NChannels/4, 4, 1));
+				kcoords 	= ones(Nchannels, 1);
+				fs 				= options.fs;
+				save(channel_filepath, 'chanMap', 'chanMap0ind', 'xcoords', 'ycoords', 'kcoords', 'fs');
+			end
 
 	otherwise
 		% do nothing
