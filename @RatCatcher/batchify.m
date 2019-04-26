63ca59578437c1c5f15853c2117ede09e754908d
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-26 17:37:14 -0400

    Iterate over batchscript

--- @RatCatcher/batchify.m
+++ @RatCatcher/batchify.m
@@ -22,6 +22,10 @@ function self = batchify(self)
 
   %% Add to the directory
 
+  % copy over the new batch function
+  copyfile(self.batchfuncpath, self.localpath);
+  corelib.verb('INFO', ['batch function copied to: ' self.localpath])
+
   % save file names and cell numbers in a text file to be read out by the script
   % this format is a standard -- it will be referenced in the batch function as well
 
@@ -32,47 +36,46 @@ function self = batchify(self)
 
   corelib.verb('INFO', 'filenames and filecodes parsed')
 
-  % copy over the new batch function
-  copyfile(self.batchfuncpath, self.localpath);
-
-  corelib.verb('INFO', ['batch function copied to: ' self.localpath])
 
   %% Copy over the generic script and rename
 
   % find the dummy script by using a lazy hack
   dummyScriptPath = which(self.batchscriptpath);
-  % name the batch script using the same format as the filenames and filecodes
-  finalScriptPath = fullfile(self.localpath, ['batchscript-', self.batchname, '.sh']);
-  copyfile(dummyScriptPath, finalScriptPath);
 
-  corelib.verb('INFO', ['batch script copied to: ' finalScriptPath])
+  for ii = 1:size(self.batchname, 1)
 
-  %% Edit the copied batch file
+    % name the batch script using the same format as the filenames and filecodes
+    finalScriptPath = fullfile(self.localpath, ['batchscript-', self.batchname{ii}, '.sh']);
+    copyfile(dummyScriptPath, finalScriptPath);
+    corelib.verb('INFO', ['batch script copied to: ' finalScriptPath])
 
-  % useful variables
-  script    = filelib.read(finalScriptPath);
-  outfile   = fullfile(self.remotepath, ['output-', self.batchname, '-', 'SGE_TASK_ID', '.csv']);
+    %% Edit the copied batch file
 
-  % TODO: make outfile more robust (accept more output types)
+    % useful variables
+    script    = filelib.read(finalScriptPath);
+    outfile   = fullfile(self.remotepath, ['output-', self.batchname{ii}, '-', 'SGE_TASK_ID', '.csv']);
 
-  % determine the name of the job array
-  script    = strrep(script, 'BATCH_NAME', self.batchname);
+    % TODO: make outfile more robust (accept more output types)
 
-  % determine the project name on the cluster
-  script    = strrep(script, 'PROJECT_NAME', project);
+    % determine the name of the job array
+    script    = strrep(script, 'BATCH_NAME', self.batchname{ii});
 
-  % determine the number of jobs
-  script    = strrep(script, 'NUM_FILES', num2str(length(self.filenames)));
+    % determine the project name on the cluster
+    script    = strrep(script, 'PROJECT_NAME', project);
 
-  % determine the argument to MATLAB
-  script    = strrep(script, 'ARGUMENT', ['$SGE_TASK_ID' ', ' ...
-                    tt self.remotepath tt ', ' ...
-                    tt self.batchname tt ', ' ...
-                    tt outfile tt ', ' ...
-                    'false']);
+    % determine the number of jobs
+    script    = strrep(script, 'NUM_FILES', num2str(length(self.filenames)));
 
-  % write to file
-  filelib.write(finalScriptPath, script);
+    % determine the argument to MATLAB
+    script    = strrep(script, 'ARGUMENT', ['$SGE_TASK_ID' ', ' ...
+                      tt self.remotepath tt ', ' ...
+                      tt self.batchname{ii} tt ', ' ...
+                      tt outfile tt ', ' ...
+                      'false']);
+
+    % write to file
+    filelib.write(finalScriptPath, script);
+  end
 
   corelib.verb('INFO', 'batch script edited')
   corelib.verb('INFO', ['run this: $ qsub ' self.remotepath filesep 'batchscript-', self.batchname, '.sh'])
