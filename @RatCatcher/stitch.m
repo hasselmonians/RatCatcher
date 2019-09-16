2724a9102c11782eec829559e95e2fdf49f04dec
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-16 14:53:28 -0400

    Add documentation

--- @RatCatcher/stitch.m
+++ @RatCatcher/stitch.m
@@ -1,18 +1,17 @@
-function data = stitch(self, data)
+function new_table = stitch(self, old_table)
 
   % stitches parsed filenames and cell numbers into datasets in the form of a table
   % if no dataset is specified, it creates a new table
 
   % Arguments:
-    % experimenter: expects either 'Caitlin' or 'Holger'
-    % alpha: the alphanumeric identifier for the experimentalist's data
-    % for experimenter = 'Caitlin', this should be an ID from cluster_info.mat
-    % e.g. 'A' or 'B', etc.
-    % data: m x n table, the data table (probably from RatCatcher.gather)
+    % self: an object of the RatCatcher class
+    %   the `filenames` and `filecodes` properties need to be well-formed
+    %   this is checked at the beginning of this function by running `validate()`
+    % old_table: m x n table, the data table (probably from RatCatcher.gather)
     %   this is an optional argument
   % Outputs:
-    % data: m x n+2 table, the data table
-
+    % new_table: m x n+2 table, the data table with the filenames and filecodes added
+    %   if concatenating the two tables fails, only the m x 2 new table is returned
 
   % check to make sure that your object is up-to-date
   self = self.validate;
