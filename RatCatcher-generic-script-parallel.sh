cb32288d27ab01d540e2875ecb07853b36956e07
Alec Hoyland <alec.hoyland@posteo.net>
2019-10-07 14:29:46 -0400

    Add generic script with parallization

---
+++ RatCatcher-generic-script-parallel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#$ -pe omp 16
+#$ -o log
+#$ -e err
+#$ -P PROJECT_NAME
+#$ -N BATCH_NAME
+#$ -l h_rt=24:00:00
+##$ -t 1-NUM_BINS
+
+module load matlab/2018a
+
+export SGE_TOTAL_BINS=NUM_BINS
+
+matlab -nodisplay -singleCompThread -r "batchFunction_parallel(ARGUMENT); exit;"
