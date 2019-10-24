f5ea1cae0fe8b64843ebcbd9a7443150fa27518b
Alec Hoyland <alec.hoyland@posteo.net>
2019-10-24 11:09:30 -0400

    Remove parpool from inside function

--- @RatCatcher/getParallelOptions.m
+++ @RatCatcher/getParallelOptions.m
@@ -13,10 +13,8 @@
 % Outputs:
 %   bin_start: index in the filename list where the bin should start
 %   bin_finish: index in the filename list where the bin ends
-%   pc: the parallel pool cluster object
-%   parpool_tmpdir: where the job files are being stored
 
-function [bin_start, bin_finish, pc, parpool_tmpdir] = getParallelOptions(bin_id, bin_total, location, batchname)
+function [bin_start, bin_finish] = getParallelOptions(bin_id, bin_total, location, batchname)
 
   % get the total number of files
   total_files = length(filelib.read(fullfile(location, ['filenames-', batchname, '.txt']))) - 1;
@@ -29,19 +27,5 @@ function [bin_start, bin_finish, pc, parpool_tmpdir] = getParallelOptions(bin_id
 
   % get the ending point of the bin
   bin_finish = bin_size * bin_id;
-
-  % set up 'local' parallel pool cluster
-  pc = parcluster('local');
-
-  % discover the number of available cores assigned by SGE
-  nCores = str2num(getenv('NSLOTS'));
-
-  % set up directory for temporary parallel pool files
-  parpool_tmpdir = ['~/.matlab/local_cluster_jobs/ratcatcher/ratcatcher_' bin_id];
-  mkdir(parpool_tmpdir);
-  pc.JobStorageLocation = parpool_tmpdir;
-
-  % start parallel pool
-  parpool(pc, nCores);
 
 end % function
