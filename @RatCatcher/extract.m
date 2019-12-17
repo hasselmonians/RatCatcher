884e0dc5c76480657254db808343e59a0eb72199
Alec Hoyland <alec.hoyland@posteo.net>
2019-12-17 13:34:06 -0500

    Add preprocess_fcn argument

--- @RatCatcher/extract.m
+++ @RatCatcher/extract.m
@@ -1,4 +1,4 @@
-function [protocolObject, dataObject] = extract(dataTable, index, protocol, verbose)
+function [protocolObject, dataObject] = extract(dataTable, index, protocol, preprocess_fcn, verbose)
   % extracts the raw data and builds an protocol object
   % Arguments:
     % dataTable: the table built by RatCatcher.gather where the data information are stored
@@ -6,6 +6,9 @@ function [protocolObject, dataObject] = extract(dataTable, index, protocol, verb
       % if not, the index argument indexes for you
     % protocol: a character vector that describes which protocol object to build
     % index: a scalar index that tells you how to index the datafile
+    % preprocess_fcn: a function handle that operates on dataTable.filenames(index) before loading the data
+    %   example: preprocess_fcn = @(x) strrep(x, 'projectnb', 'mnt')
+    % verbose: a boolean flag for how much info text to print
   % Outputs:
     % protocolObject: the struct produced by the protocol method
     % dataObject: the root (Session) object specified by the dataTable and index
@@ -18,16 +21,23 @@ function [protocolObject, dataObject] = extract(dataTable, index, protocol, verb
     assert(exist('index', 'var') && isscalar(index), 'If dataTable is an m x n table, then index should be a positive integer.')
   end
 
-  if nargin < 3
-      protocol = 'BandwidthEstimator';
-      verbose  = false;
-  elseif isempty(protocol)
+  if ~exist('protocol', 'var') || isempty(protocol)
       protocol = 'BandwidthEstimator';
   end
 
+  if ~exist('verbose', 'var') || isempty(verbose)
+      verbose = false;
+  end
+
   % load the data file
-  if verbose, disp('[INFO] load the data file'); end
-  load(dataTable.filenames{index});
+  corelib.verb(verbose, 'RatCatcher::extract', 'loading the data file')
+
+  if ~exist('preprocess_fcn') || isempty(preprocess_fcn)
+      load(dataTable.filenames{index});
+  else
+      load(preprocess_fcn(dataTable.filenames{index}));
+  end
+
   root            = root.AppendKalmanVel;
   root            = root.FixTime;
   dataObject      = root;
@@ -36,6 +46,7 @@ function [protocolObject, dataObject] = extract(dataTable, index, protocol, verb
 
   % determine next step based on protocol method
   corelib.verb(verbose, 'RatCatcher::extract', 'setting up protocol object')
+
   switch protocol
   case 'BandwidthEstimator'
     protocolObject = BandwidthEstimator(dataObject);
