4aedc83ff19497a56dbc8cbcdf573b38cf639602
Alec Hoyland <ahoyland@bu.edu>
2020-02-04 16:01:45 -0500

    Use new 'mode' property

--- @RatCatcher/getBatchScriptPath.m
+++ @RatCatcher/getBatchScriptPath.m
@@ -1,10 +1,11 @@
 function batchscriptpath = getBatchScriptPath(self)
   % determine the batch script name
 
-  if self.parallel == false
-    batchscriptpath = which('RatCatcher-generic-script.sh');
-  else
-    batchscriptpath = which('RatCatcher-generic-script-parallel.sh');
+  switch self.mode
+  case 'parallel'
+      batchscriptpath = which('RatCatcher-generic-script.sh');
+  otherwise
+      batchscriptpath = which('RatCatcher-generic-script-parallel.sh');
   end
 
   assert(~isempty(batchscriptpath), 'batch script path not found, is it on the MATLAB path?')
