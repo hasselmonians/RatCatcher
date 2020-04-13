d3313bc4083440f2b9c1a92ec21d7f2588410d42
Alec Hoyland <alec.hoyland@posteo.net>
2020-04-13 11:45:17 -0400

    Add function to set paths

---
+++ +RatCatcher/init.m
@@ -0,0 +1,25 @@
+function init(isCluster)
+
+    % sets the matlab path for the RatCatcher project
+
+    if ~exist('isCluster', 'var')
+        isCluster = false;
+    end
+
+    % e.g. ~/code
+    code_dir = pathlib.strip(mfilename('fullpath'), 3);
+
+    if isCluster
+        addpath /projectnb/hasselmogrp/ahoyland/scripts
+        addpath /projectnb/hasselmogrp/ahoyland/srinivas.gs_mtools
+    else
+        addpath(code_dir)
+        addpath(fullfile(code_dir, 'srinivas.gs_mtools'))
+    end
+
+    addpath(fullfile(code_dir, 'RatCatcher'))
+    addpath(fullfile(code_dir, 'RatCatcher', 'batchfunctions'))
+    addpath(fullfile(code_dir, 'RatCatcher', 'natsort'))
+    addpath(fullfile(code_dir, 'RatCatcher', 'scripts'))
+
+end % function
