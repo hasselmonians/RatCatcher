31138d606da862ac1bb74fbd410fdcb22986e71f
Alec Hoyland <alec.hoyland@posteo.net>
2020-04-13 13:04:38 -0400

    Update test script

--- test/script.m
+++ test/script.m
@@ -1,8 +1,17 @@
-% NOTE: you will need to change the following path to where you installed RatCatcher locally
+%% Test Script
+
+% This script contains a demo for how to use RatCatcher.
+% It will run a trivial MATLAB function on the cluster,
+% that copies a matrix.
+
+% find the path to the RatCatcher directory
 RatCatcher_path = pathlib.strip(mfilename('fullpath'), 2);
+
+%% Create the RatCatcher object
 
 r = RatCatcher;
 
+% identify the experimenter as "test"
 r.expID       = 'test';
 
 % NOTE: you will need to change the following paths to make sense for you
@@ -19,8 +28,19 @@ r.localpath   = '/mnt/hasselmogrp/ahoyland/RatCatcher/cluster';
 r.protocol    = 'Test';
 r.project     = 'hasselmogrp';
 
-% Manually add the test data
-r.filenames   = {fullfile(RatCatcher_path, 'test', 'data', 'datafile_1.csv'); ...
-                fullfile(RatCatcher_path, 'test', 'data', 'datafile_2.csv')};
+%% Manually add the test data
+
+% local, relative paths to the raw data files in the RatCatcher directory
+data_paths    = {fullfile('test', 'data', 'datafile_1.csv'),...
+                fullfile('test', 'data', 'datafile_2.csv')};
+
+% local paths to the raw data files on the cluster
+r.filenames   = strcat(r.remotepath, data_paths);
+
+% copy the raw data files to the cluster location
+% in general, you won't need to do this
+% this is necessary here because of how contrived this example is
+copyfile(fullfile(RatCatcher_path, data_paths{1}), r.localpath);
+copyfile(fullfile(RatCatcher_path, data_paths{2}), r.localpath);
 
 r = r.batchify;
